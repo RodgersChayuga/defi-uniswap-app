5239f8aea7af568c36734b77db71a9a7a33b6712
Rodgers Chayuga <chayugarodgers@gmail.com>
2024-03-07 01:22:10 +0300

    Smart-contract: Deploy Transactions smart contract.

--- smart_contract/hardhat.config.ts
+++ smart_contract/hardhat.config.ts
@@ -2,7 +2,15 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.24",
+  solidity: "0.8.17",
+  networks: {
+    sepolia: {
+      url: "https://eth-sepolia.g.alchemy.com/v2/vEdRIZ1LCKRqRPPIWtBFcmVaaeRe8ob5",
+      accounts: [
+        "59a22f27341c75048b6f62b3c00c5107d6247b4a632757ebbb430423df60562d",
+      ],
+    },
+  },
 };
 
 export default config;
